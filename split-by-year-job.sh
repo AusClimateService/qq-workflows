5bb17fa52aa05e0b4bf9b61333f6e7393ed47e22
Damien Irving <irving.damien@gmail.com>
2023-09-13 14:12:47 +1000

    Split output files by year

---
+++ split-by-year-job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -P wp00
+#PBS -q normal
+#PBS -l walltime=2:00:00
+#PBS -l mem=10GB
+#PBS -l storage=gdata/xv83+gdata/ia39
+#PBS -l wd
+#PBS -l ncpus=5
+
+module load cdo
+
---
+++ split-by-year.sh
@@ -0,0 +1,27 @@
+#
+# Bash script for separating bias corrected data into single years
+#
+# Usage: bash split-by-year.sh {full/path/to/file1.nc ... full/path/to/fileN.nc }
+#
+
+for infile in "$@"; do
+    experiment=`basename ${infile} | cut -d _ -f 4`
+    dates=`basename ${infile} | cut -d _ -f 9`
+    start=`echo ${dates:0:4}`
+    end=`echo ${dates:9:4}`
+    years=($(seq ${start} 1 ${end}))
+    for year in "${years[@]}"; do
+        outfile=`echo ${infile} | sed s:xv83/dbi599:ia39:g | sed s:${start}:${year}:g | sed s:${end}:${year}:g`
+        if [ ${year} -lt 2015 ]; then
+           outfile=`echo ${outfile} | sed s:${experiment}:historical:g`
+        fi
+        outdir=`dirname ${outfile}`
+        mkdir -p ${outdir} 
+        command="cdo seldate,${year}-01-01,${year}-12-31 ${infile} ${outfile}"
+        jobfile=split-by-year-job-${year}.sh
+        cp split-by-year-job.sh ${jobfile}
+        echo ${command} >> ${jobfile}
+        qsub ${jobfile}  
+    done
+done
+
