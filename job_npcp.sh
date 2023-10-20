2f75621bb2c81d99c5e63d58727eb0b75ba820e7
Damien Irving <irving.damien@gmail.com>
2023-10-20 13:49:37 +1100

    Add more resources

--- job_npcp.sh
+++ job_npcp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P xv83
 #PBS -q normalbw
-#PBS -l walltime=5:00:00
-#PBS -l mem=20GB
+#PBS -l walltime=7:00:00
+#PBS -l mem=40GB
 #PBS -l storage=gdata/ia39+gdata/xv83+gdata/wp00
 #PBS -l wd
 #PBS -v method,var,task,rcm,gcm
