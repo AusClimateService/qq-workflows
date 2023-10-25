47c2ad1fce9ce5fcbd9178c23941be07fff4138b
Damien Irving <irving.damien@gmail.com>
2023-10-25 17:06:14 +1100

    Add linear interp

--- npcp.sh
+++ npcp.sh
@@ -4,7 +4,7 @@
 #             
 
 method_list=(ecdfm qdm)
-var_list=(tasmin tasmax pr)
+var_list=(tasmax tasmin pr)
 rcm_list=(BOM-BARPA-R UQ-DES-CCAM-2105 CSIRO-CCAM-2203)
 gcm_list=(ECMWF-ERA5 CSIRO-ACCESS-ESM1-5)
 for var in "${var_list[@]}"; do
@@ -19,6 +19,11 @@ for var in "${var_list[@]}"; do
                     task_list+=(historical)
                 fi
                 for task in "${task_list[@]}"; do
+                    if [ "${var}" == "pr" ] && [ "${method}" == "ecdfm" ] ; then
+                        interp=linear
+                    else
+                        interp=nearest
+                    fi
                     if [ "${var}" == "pr" ] && [ "${method}" == "qdm" ] ; then
                         if [ "${task}" == "xvalidation" ] && [ "${gcm}" == "ECMWF-ERA5" ] ; then
                             nquantiles=500
@@ -28,7 +33,7 @@ for var in "${var_list[@]}"; do
                     else
                         nquantiles=100
                     fi
-                    qsub -v method=${method},var=${var},task=${task},rcm=${rcm},gcm=${gcm},nquantiles=${nquantiles} job_npcp.sh
+                    qsub -v method=${method},var=${var},task=${task},rcm=${rcm},gcm=${gcm},nquantiles=${nquantiles},interp=${interp} job_npcp.sh
                 done
             done
         done
