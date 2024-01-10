61aa0a36358ef05c68063e8c5dc2b0efe62a3c8d
Damien Irving <irving.damien@gmail.com>
2024-01-10 15:04:45 +1100

    Minor tweaks

--- cih/job_cih.sh
+++ cih/job_cih.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P xv83
 #PBS -q hugemem
-#PBS -l walltime=10:00:00
+#PBS -l walltime=20:00:00
 #PBS -l mem=250GB
 #PBS -l storage=gdata/xv83+gdata/wp00+gdata/oi10+gdata/fs38+gdata/dk7+gdata/ia39
 #PBS -l wd
