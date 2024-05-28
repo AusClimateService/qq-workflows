0bc279273e0b8a2b6f605ff2e755ac2aab4619e4
Damien Irving <irving.damien@gmail.com>
2024-05-28 16:57:26 +1000

    Update BARRA-R2 path

--- qdc-cmip6/config_qdc-cmip6.mk
+++ qdc-cmip6/config_qdc-cmip6.mk
@@ -182,8 +182,7 @@ else ifeq (${OBS_DATASET}, ERA5)
   TARGET_DIR=/g/data/wp00/data/observations/ERA5/${TARGET_VAR}/daily
   #TODO OUTPUT_GRID_LABEL=
 else ifeq (${OBS_DATASET}, BARRA-R2)
-#  TARGET_DIR=/g/data/ob53/BARRA2/output/reanalysis/AUS-11/BOM/ERA5/historical/hres/BARRA-R2/v1/day/${TARGET_VAR}/v20231001
-  TARGET_DIR=/scratch/hd50/jt4085/bias_correction/BARRA2/output/reanalysis/AUS-11/BOM/ERA5/historical/hres/BARRA-R2/v1/day/${TARGET_VAR}/v20240516
+  TARGET_DIR=/g/data/ob53/BARRA2/output/reanalysis/AUS-11/BOM/ERA5/historical/hres/BARRA-R2/v1/day/${TARGET_VAR}/v20240516
   OUTPUT_GRID_LABEL=AUS-11
   TARGET_DROP_VARS=--drop_vars sigma level_height model_level_number
 endif
