458e622cccb06219974010f92d457013033b2507
Damien Irving <irving.damien@gmail.com>
2024-06-05 11:51:00 +1000

    Fix bug

--- qdc-cmip6/split-by-year.sh
+++ qdc-cmip6/split-by-year.sh
@@ -4,7 +4,7 @@
 # Usage: bash split-by-year.sh {full/path/to/file1.nc ... full/path/to/fileN.nc }
 #
 # Example path:
-# /g/data/wp00/data/QQ-CMIP6/ACCESS-ESM1-5/ssp245/r6i1p1f1/day/rsds/rsds_day_ACCESS-ESM1-5_ssp245_r6i1p1f1_AUS-r005_2035-2064_qdm-additive-monthly-q100-nearest_ERA5-1985-2014_historical-1985-2014.nc
+# /g/data/wp00/data/QQ-CMIP6/ACCESS-ESM1-5/ssp245/r6i1p1f1/day/rsds/2035-2064/rsds_day_ACCESS-ESM1-5_ssp245_r6i1p1f1_AUS-r005_2035-2064_qdm-additive-monthly-q100-nearest_ERA5-1985-2014_historical-1985-2014.nc
 
 
 for infile in "$@"; do
@@ -14,7 +14,7 @@ for infile in "$@"; do
     end_year=`echo ${date_label} | rev | cut -d - -f 1 | rev`
     years=($(seq ${start_year} 1 ${end_year}))
     for year in "${years[@]}"; do
-        outfile=`echo ${infile} | sed s:${start_year}-${end_year}:${year}:g`
+        outfile=`echo ${infile} | sed s:${start_year}-${end_year}_:${year}_:g`
         outdir=`dirname ${outfile}`
         mkdir -p ${outdir}
         qsub -v year=${year},infile=${infile},outfile=${outfile} /g/data/xv83/quantile-mapping/qq-workflows/qdc-cmip6/split-by-year-job.sh
