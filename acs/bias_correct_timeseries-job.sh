13d7ea224290dbb6dbf07a01f5a4ec7b47156b98
Damien Irving <irving.damien@gmail.com>
2024-06-07 17:18:22 +1000

    increase time

--- acs/bias_correct_timeseries-job.sh
+++ acs/bias_correct_timeseries-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P wp00
 #PBS -q hugemem
-#PBS -l walltime=30:00:00
+#PBS -l walltime=40:00:00
 #PBS -l mem=350GB
 #PBS -l storage=gdata/xv83+gdata/ia39+gdata/ob53+gdata/hq89
 #PBS -l wd
