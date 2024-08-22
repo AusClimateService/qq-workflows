d8674f35deae573884c323c71fe39bb3f0478e54
Damien Irving <irving.damien@gmail.com>
2024-08-22 16:58:30 +1000

    Update template notebooks

---
+++ qdc-cmip6/job_model_bias.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -P xv83
+#PBS -q hugemem
+#PBS -l walltime=5:00:00
+#PBS -l mem=400GB
+#PBS -l storage=gdata/xv83+gdata/ob53+gdata/fs38+gdata/ia39
+#PBS -l wd
+#PBS -v model
+
+# To run: qsub -v model=ACCESS-CM2 job_model_bias.sh
+
+/g/data/xv83/dbi599/miniconda3/envs/qqscale/bin/papermill model_bias.ipynb model_bias_${model}.ipynb -p model ${model}
